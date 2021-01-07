a2d6c92eea76fb196582a37386140d322446bf85
IljaRukin <ilja.rukin@gmail.com>
2021-01-07 19:46:24 +0100

    first commit

---
+++ ping_log/connectivity.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+p1="$( ping -c5 192.168.0.1 | grep "packet loss" | sed "s/.*received, //" | sed "s/%.*time /,/" | sed "s/ms//" | sed "s/+5 errors, //g")"
+p2="$( ping -c5 1.1.1.1 | grep "packet loss" | sed "s/.*received, //" | sed "s/%.*time /,/" | sed "s/ms//" | sed "s/+5 errors, //g")"
+p3="$( ping -c5 8.8.8.8 | grep "packet loss" | sed "s/.*received, //" | sed "s/%.*time /,/" | sed "s/ms//" | sed "s/+5 errors, //g")"
+
+t="$(date +%d-%m-%Y_%H:%M:%S)"
+
+if [ ${#p1} -lt 3 ]
+  then
+    p1="100,100"
+fi
+if [ ${#p2} -lt 3 ]
+  then
+    p2="100,100"
+fi
+if [ ${#p3} -lt 3 ]
+  then
+    p3="100,100"
+fi
+
+echo "$t,$p1,$p2,$p3" >> /var/www/html/ping_log/ping_log.csv
